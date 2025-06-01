f999c8d841c10ba2b3042f8962ecf0bf78f20d09
Abdulelah Mulla <aehmul@yahoo.com>
2025-06-01 14:40:36 +0300

    Added ios client

---
+++ client/ios/iosclient/iosclient/iosclientApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosclientApp.swift
+//  iosclient
+//
+//  Created by Abdulelah Mulla on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct iosclientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ client/ios/iosclient/iosclientTests/iosclientTests.swift
@@ -0,0 +1,17 @@
+//
+//  iosclientTests.swift
+//  iosclientTests
+//
+//  Created by Abdulelah Mulla on 6/1/25.
+//
+
+import Testing
+@testable import iosclient
+
+struct iosclientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
