b39c79fd6f56e215304f935337d1130a5baf0e00
Abdulelah Mulla <aehmul@yahoo.com>
2025-06-03 22:23:03 +0300

    Added Camera Stream

---
+++ client/ios/swift/iosclient/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iosclient
+//
+//  Created by Abdulelah Mulla on 6/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack{
+            VStack {
+                Text("Hello, world!")
+                Text("To begin, please navigate to the camera stream option.")
+                
+            }
+            .padding()
+            NavigationLink("Camera Stream", destination: CameraStreamView())
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
