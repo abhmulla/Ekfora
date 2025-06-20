50dc39fdd9b88c6b3360483d43b0b06c658c45d4
Abdulelah Mulla <aehmul@yahoo.com>
2025-06-21 01:31:27 +0300

    Created livestream

--- server/server.py
+++ server/server.py
@@ -1,5 +1,7 @@
 import websockets
 import asyncio
+import cv2
+import numpy as np
 
 PORT = 7890
 
@@ -9,11 +11,28 @@ async def echo(websocket):
     print("Client connected")
     async for message in websocket:
         print("Message received from client")
-        await websocket.send("Pong " + message)
+        
+        message = np.frombuffer(message, dtype=np.uint8)
+
+        # Decode JPEG image to OpenCV format
+        frame = cv2.imdecode(message, cv2.IMREAD_COLOR)
+
+        if frame is None:
+            continue  
+
+        cv2.imshow("Live JPEG Stream", frame)
+
+        # Wait a short time — adjust for desired FPS
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 async def main():
-    async with websockets.serve(echo, "0.0.0.0", PORT):
-        await asyncio.Future()  # Run foreve
+    try:
+        async with websockets.serve(echo, "0.0.0.0", PORT):
+            await asyncio.Future() 
+    except KeyboardInterrupt:
+            cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
